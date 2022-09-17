371fa7776dd6fce86d1ad2f04423e60b1bb71363
Muhammed Sari <muhammed@dive.be>
2022-09-17 21:44:41 +0200

    IoC container is smart enough for autowiring

--- src/ExpoServiceProvider.php
+++ src/ExpoServiceProvider.php
@@ -3,7 +3,6 @@
 namespace NotificationChannels\Expo;
 
 use Illuminate\Contracts\Config\Repository;
-use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Notifications\ChannelManager;
 use Illuminate\Support\ServiceProvider;
@@ -20,21 +19,7 @@ final class ExpoServiceProvider extends ServiceProvider
     {
         $this->app->afterResolving(ChannelManager::class, $this->extendManager(...));
         $this->app->bind(ExpoGateway::class, $this->createExpoGateway(...));
-        $this->app->singleton(ExpoChannel::class, $this->createExpoChannel(...));
-    }
-
-    /**
-     * Create a new ExpoChannel instance.
-     */
-    private function createExpoChannel(Application $app): ExpoChannel
-    {
-        /** @var ExpoGateway $gateway */
-        $gateway = $app->make(ExpoGateway::class);
-
-        /** @var Dispatcher $events */
-        $events = $app->make(Dispatcher::class);
-
-        return new ExpoChannel($gateway, $events);
+        $this->app->singleton(ExpoChannel::class);
     }
 
     /**
